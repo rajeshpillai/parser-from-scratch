2c15cd11e9f7d6bd81a68039ae5909f8428a8c2a
rajeshpillai <pillai.rajesh@gmail.com>
2023-10-23 20:01:55 +0530

    A simple test runner

--- __tests__/run.js
+++ __tests__/run.js
@@ -1,44 +1,37 @@
+const assert = require('assert');
 const {Parser} = require('../src/parser');
+
+const tests = [require('./literals-test.js')]
 
 const parser = new Parser();
 
 // NOTE:  The program variable is overwritten in each test case.
 // The last one is the active test program
 let program = "";
-program = `   42  `;       // This is number
-program = `   "  42    " `;   // This is string token
-program = '"42"';
-program = `'42'`;  // Assignment: single quoted string
-
-// Single line comment
-program = `
-  // Number
-  42
- `;  
-
- // Multi line comment
-program = `
-  /* 
-   Documentation comment:
-  */
-   42
- `;  
 
 
- //TODO:   Multiple expression test
-program = `
-/**
- Documentation comment:
-*/
-"hello";
-// Number: 
- 42;
-`;  
+// For manual test
+function exec() {
+  program = `
+    /**
+     Documentation comment:
+    */
+    "hello";
+    // Number: 
+    42;
+  `;  
+  const ast = parser.parse(program);
+  console.log(JSON.stringify(ast, null, 2));
+}
 
 
+// Test function 
 
+function test (program, expected) {
+  const ast = parser.parse(program);
+  assert.deepEqual(ast, expected);
+}
 
-const ast = parser.parse(program);
-
-console.log(JSON.stringify(ast, null, 2));
-
+// Run all test 
+tests.forEach(testRun => testRun(test));
+console.log('All tests passed!');
