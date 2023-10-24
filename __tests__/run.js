fb55e68c7294de67fbe7fcf5e0a28e53300bc112
rajeshpillai <pillai.rajesh@gmail.com>
2023-10-24 16:12:07 +0530

    Iteration Statement - Do While

--- __tests__/run.js
+++ __tests__/run.js
@@ -15,6 +15,7 @@ const tests = [
   require('./logical-test.js'),
   require('./unary-test.js'),
   require('./while-test.js'),
+  require('./do-while-test.js'),
 ];
 
 const parser = new Parser();
@@ -23,64 +24,9 @@ const parser = new Parser();
 // For manual test
 function exec() {
   const program = `
-      let y;
-      let a, b;
-      let c, d = 10;
-      let foo = bar = 10;   // let foo = (bar = 10);
-      r = 10;
-      
-      let x = 42;
-
-      // if/esle
-      if (x) {
-        x = 0;
-      } else {
-        x += 1;
-      }
-
-      // only if 
-      if (x) {
-        x = 0;
-      }
-
-      // Shortcut
-      if (x) x = 0;
-
-      // Nested if
-      if(x) if(y) {} else {};
-      if(x) if(y) {} else {} else {};
-
-      // Relational expression and equality
-      if (x + 5 > 10)  {
-        x = 10;
-      } else {
-        x += 1;
-      }
-
-      if (x + 5 >= 10)  {
-        x = 10;
-      } else {
-        x += 1;
-      }
-
-      x + 5 > 10;  // (x + 5) > 10
-
-      // Relational expression and equality
-      x + 5 > 10 == true; // (x + 5) > 10 == true
-
-      // Logical && has lower precedence than relational
-      x > 5 && y < 10; // (x > 5) && (y < 10)
-
-      x > 5 || y < 10; // (x > 5) || (y < 10)
-
-      // Unary operator 
-      !x; 
-      -x;
-
-      while (x > 10) {
+      do {
         x -= 1;
-      }
-
+      } while(x > 10);
   `;  
   const ast = parser.parse(program);
   console.log(JSON.stringify(ast, null, 2));
